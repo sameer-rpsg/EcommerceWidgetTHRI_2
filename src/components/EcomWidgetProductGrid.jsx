03c3f8549dfcc345f41c126da057426207fb32f8
sameer-rpsg <sameer.zerror@gmail.com>
2025-11-03 16:46:16 +0530

    content changed

--- src/components/EcomWidgetProductGrid.jsx
+++ src/components/EcomWidgetProductGrid.jsx
@@ -9,13 +9,13 @@ const EcomWidgetProductGrid = () => {
       <div
         className={`${styles.EcomWidgetGridWrapper} ${styles.EcomWidgetProductGridWrapper}`}
       >
-        <EcomWidgetCard imgSrc="https://www.hollywoodreporterindia.com/_next/image?url=https%3A%2F%2Fcdn.hollywoodreporterindia.com%2Fbms-new%2Feditor-images%2F-2024-09-23T07%253A37%253A20.907Z-Method-Dressing-New3.jpg&w=3840&q=75" />
+        <EcomWidgetCard imgSrc="https://www.hollywoodreporterindia.com/_next/image?url=https%3A%2F%2Fcdn.hollywoodreporterindia.com%2Fbms-new%2Feditor-images%2F-2024-09-23T07%253A36%253A16.730Z-Ananya.jpg&w=3840&q=75" />
         <div className={styles.EcomPrdGrid}>
           <div className={styles.EcomPrdGridImgCntr}>
-            <Image
+            <img
               width={1000}
               height={1000}
-              src="/dfubt0jl0fqb6awf06ue.webp"
+              src="https://static.zara.net/assets/public/9b9c/5aaf/204042e2b4d2/0ba273dfd0b6/04772239800-e1/04772239800-e1.jpg?ts=1759482412033&w=723"
               alt=""
             />
             <div className={styles.EcomWidgetcard_contentWrapper}>
@@ -27,10 +27,10 @@ const EcomWidgetProductGrid = () => {
             </div>
           </div>
           <div className={styles.EcomPrdGridImgCntr}>
-            <Image
+            <img
               width={1000}
               height={1000}
-              src="/q5rmlpgcmfnaqcoimgtu.webp"
+              src="https://static.zara.net/assets/public/9429/8494/19154ed88872/062badd56a31/05919209800-e1/05919209800-e1.jpg?ts=1760457107024&w=1115"
               alt=""
             />
              <div className={styles.EcomWidgetcard_contentWrapper}>
@@ -42,10 +42,10 @@ const EcomWidgetProductGrid = () => {
             </div>
           </div>
           <div className={styles.EcomPrdGridImgCntr}>
-            <Image
+            <img
               width={1000}
               height={1000}
-              src="/t2tydn9jsxbrb5oh82hs.webp"
+              src="https://static.zara.net/assets/public/6cac/e0ab/7e334e92a63b/7460501ed045/00085827800-e1/00085827800-e1.jpg?ts=1755788161355&w=723"
               alt=""
             />
              <div className={styles.EcomWidgetcard_contentWrapper}>
@@ -57,10 +57,10 @@ const EcomWidgetProductGrid = () => {
             </div>
           </div>
           <div className={styles.EcomPrdGridImgCntr}>
-            <Image
+            <img
               width={1000}
               height={1000}
-              src="/jn0euaqz2i0yhqw7usww.webp"
+              src="https://static.zara.net/assets/public/bf95/520a/3d5945f6b174/7820d3e27ba6/01223452700-e1/01223452700-e1.jpg?ts=1759392195051&w=723"
               alt=""
             />
             <div className={styles.EcomWidgetcard_contentWrapper}>
